a46ac65639524ae832f164f0bc6157fabb851311
Rodrigo <rodrigo@jesus.(none)>
2013-02-04 22:35:37 -0800

    search

--- js/main.js
+++ js/main.js
@@ -1,1 +1,43 @@
+$(document).ready(function(){
+	/* search */
+	var searchActive = false;
 
+	function searchOn() {
+		$('.search').show();
+		$('.search-input').focus();
+		searchActive = true;
+	}
+
+	function searchOff() {
+		$('.search').hide();
+		$('.search-input').blur();
+		$('.search-input').val('');
+		searchActive = false;
+	}
+
+	$('.search-link').click(function(){
+		searchOn();
+	});
+
+	$('.search').click(function(event){
+		if(!$(event.target).is('input')) {
+			searchOff();
+		}
+	});
+
+	$(document).keyup(function(event) {
+		if (event.keyCode == 27) { // esc
+			searchOff();
+		}
+	});
+
+	$(document).keypress(function(event) {
+		if (!searchActive) {
+			var char = String.fromCharCode(event.keyCode);
+			if (char.match(/\w/)) {
+				searchOn();
+			}
+		}
+	});
+
+});
